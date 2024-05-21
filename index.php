767e21bb15d56baaabcc4c35d94bd353f5733a22
yleniapregnolato <yleniapregnolato@gmail.com>
2024-05-21 16:21:08 +0200

    add: print hotels array

---
+++ index.php
@@ -0,0 +1,63 @@
+<?php
+
+    $hotels = [
+
+        [
+            'name' => 'Hotel Belvedere',
+            'description' => 'Hotel Belvedere Descrizione',
+            'parking' => true,
+            'vote' => 4,
+            'distance_to_center' => 10.4
+        ],
+        [
+            'name' => 'Hotel Futuro',
+            'description' => 'Hotel Futuro Descrizione',
+            'parking' => true,
+            'vote' => 2,
+            'distance_to_center' => 2
+        ],
+        [
+            'name' => 'Hotel Rivamare',
+            'description' => 'Hotel Rivamare Descrizione',
+            'parking' => false,
+            'vote' => 1,
+            'distance_to_center' => 1
+        ],
+        [
+            'name' => 'Hotel Bellavista',
+            'description' => 'Hotel Bellavista Descrizione',
+            'parking' => false,
+            'vote' => 5,
+            'distance_to_center' => 5.5
+        ],
+        [
+            'name' => 'Hotel Milano',
+            'description' => 'Hotel Milano Descrizione',
+            'parking' => true,
+            'vote' => 2,
+            'distance_to_center' => 50
+        ],
+
+    ];
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php foreach($hotels as $cur_hotel) { ?>
+        <div>
+            <h3><?php echo $cur_hotel["name"] ?></h3>
+            <p><?php echo $cur_hotel["description"] ?></p>
+            <p>PARCHEGGIO: <?php echo $cur_hotel["parking"] ?></p>
+            <p>VOTO: <?php echo $cur_hotel["vote"] ?></p>
+            <p>DISTANZA DAL CENTRO: <?php echo $cur_hotel["distance_to_center"] ?></p>
+        </div>
+    <?php } ?>
+</body>
+</html>
